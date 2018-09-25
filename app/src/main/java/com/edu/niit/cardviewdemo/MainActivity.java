811d51c848d48c5306afe89251c465393be992ba
unknown <834914153@qq.com>
2018-09-25 17:43:48 +0800

    Initial commit

---
+++ app/src/main/java/com/edu/niit/cardviewdemo/MainActivity.java
@@ -0,0 +1,77 @@
+package com.edu.niit.cardviewdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+    private CardView mCardView;
+    private SeekBar sb1;
+    private SeekBar sb2;
+    private SeekBar sb3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        assignViews();
+    }
+
+    private void assignViews() {
+        mCardView=findViewById(R.id.tv_item);
+        sb1=findViewById(R.id.sb_1);
+        sb2=findViewById(R.id.sb_2);
+        sb3=findViewById(R.id.sb_3);
+        sb1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mCardView.setRadius(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        sb2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean fromUser) {
+                mCardView.setCardElevation(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        sb3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean fromUser) {
+                mCardView.setContentPadding(i,i,i,i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
